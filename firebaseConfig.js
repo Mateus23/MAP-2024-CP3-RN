b667799fda375073dfd1ec3f26e885a3b7e125d0
Mateus Pelluchi <mateus.pelluchi@gmail.com>
2024-05-19 17:04:09 -0300

    add firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBiOMoCVc5---M-uSS33ZwMxGP1u2beS_U",
+    authDomain: "checkpoint-3-5dd1e.firebaseapp.com",
+    projectId: "checkpoint-3-5dd1e",
+    storageBucket: "checkpoint-3-5dd1e.appspot.com",
+    messagingSenderId: "795709617646",
+    appId: "1:795709617646:web:2a4467ed99ff2cec8e4aff"
+  };
+export const app = initializeApp(firebaseConfig);
